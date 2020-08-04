dee4b8c4b1d05683cf34f0388b272c5fa0149175
Guilherme Santos <g.santos.sfc@gmail.com>
2020-08-04 17:59:45 -0300

    add props para o componente TeacherItem

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,30 +4,36 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-const TeacherItem: React.FC = () => {
+interface TeacherItemProps {
+  imgURL: string;
+  name: string;
+  subject: string;
+  description: string;
+  priceHour: number;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ imgURL, name, subject, description, priceHour }) => {
   return (
     <article className="teacher-item">
       <header>
         <img
-          src="https://avatars3.githubusercontent.com/u/16351117?s=460&u=157575a4f7bc20221f4693aa045fd800654c7016&v=4"
-          alt="Guilherme Santos"
+          src={imgURL}
+          alt={name}
         />
         <div>
-          <strong>Guilherme Santos</strong>
-          <span>Química</span>
+          <strong>{name}</strong>
+          <span>{subject}</span>
         </div>
       </header>
 
       <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br /><br />
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+        {description}
       </p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 60,00</strong>
+          <strong>{`R$ ${priceHour.toFixed(2).replace('.', ',')}`}</strong>
         </p>
         <button type="button">
           <img src={whatsappIcon} alt="Whatsapp"/>
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -37,9 +37,27 @@ const TeacherList: React.FC = () => {
       </PageHeader>
  
       <main>
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        <TeacherItem
+          imgURL="https://avatars3.githubusercontent.com/u/16351117?s=460&u=157575a4f7bc20221f4693aa045fd800654c7016&v=4"
+          name="Guilherme Santos"
+          subject="Química"
+          description="Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões."
+          priceHour={100}
+        />
+        <TeacherItem
+          imgURL="https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4"
+          name="Diego Fernandes"
+          subject="Ciências"
+          description="Instrutor de Educação Física para iniciantes, minha missão de vida é levar saúde e contribuir para o crescimento de quem se interessar. Comecei a minha jornada profissional em 2001, quando meu pai me deu dois alteres de 32kg com a seguinte condição: 'Aprenda a fazer dinheiro com isso!'"
+          priceHour={150}
+        />
+        <TeacherItem
+          imgURL="https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4"
+          name="Mayk Brito"
+          subject="Geografia"
+          description="As vezes não sei nem onde eu tô, mas consigo me localizar facilmente em qualquer lugar. Tenho memória fotográfica e nunca fico perdido. Eu ensino a galera como não se perder na vida, com lições geográficas simples pra você nunca mais precisar de mapa na sua bela vida."
+          priceHour={360}
+        />
       </main>
     </div>
   );
