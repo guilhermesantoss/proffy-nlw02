1a0d5c941b83b93e23d5eb2c63fcbce7b7edbdc0
Guilherme Santos <g.santos.sfc@gmail.com>
2020-08-05 21:26:55 -0300

    add funcionalidade de adicionar novo horario

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -5,27 +5,28 @@ import './styles.css';
 interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
   name: string;
   label: string;
+  placeholder?: string;
   options: Array<{
     value: string;
     label: string;
   }>;
 }
 
-const Select: React.FC<SelectProps> = ({ name, label, options, ...rest }) => {
+const Select: React.FC<SelectProps> = ({ name, label, options, placeholder, ...rest }) => {
   return (
     <div className="select-block">
       <label htmlFor={name}>{label}</label>
       <select
+        defaultValue=""
         id={name}
         {...rest}
       >
         <option
           value=""
           disabled
-          selected
           hidden
         >
-          Selecione qual você quer ensinar
+          {placeholder}
         </option>
         {options.map(option => (
           <option
--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
@@ -10,6 +10,17 @@ import Textarea from '../../components/Textarea';
 import Select from '../../components/Select';
 
 const TeacherForm: React.FC = () => {
+  const [scheduleItems, setScheduleItems] = useState([
+    { week_day: 0, from: '', to: ''},
+  ]);
+
+  function addNewScheduleItem() {
+    setScheduleItems([
+      ...scheduleItems,
+      { week_day: 0, from: '', to: ''},
+    ]);
+  }
+
   return (
     <div id="page-teacher-form" className="container">
       <PageHeader
@@ -45,6 +56,7 @@ const TeacherForm: React.FC = () => {
           <Select
             name="subject"
             label="Matéria"
+            placeholder="Selecione qual você quer ensinar"
             options={[
               { value: 'Artes', label: 'Artes' },
               { value: 'Biologia', label: 'Biologia' },
@@ -62,6 +74,47 @@ const TeacherForm: React.FC = () => {
             name="cost"
             label="Custo da sua hora por aula"
           />
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            Horários disponíveis
+            <button
+              type="button"
+              onClick={addNewScheduleItem}
+            >
+              + Novo Horário
+            </button>
+          </legend>
+          
+          {scheduleItems.map(scheduleItem => (
+            <div key={scheduleItem.week_day}className="schedule-item">
+              <Select
+                name="week_day"
+                label="Dia da semana"
+                placeholder="Selecione o dia"
+                options={[
+                  { value: '0', label: 'Domingo' },
+                  { value: '1', label: 'Segunda-feira' },
+                  { value: '2', label: 'Terça-feira' },
+                  { value: '3', label: 'Quarta-feira' },
+                  { value: '4', label: 'Quinta-feira' },
+                  { value: '5', label: 'Sexta-feira' },
+                  { value: '6', label: 'Sábado' },
+                ]}
+              />
+              <Input
+                name="from"
+                label="Das"
+                type="time"
+              />
+              <Input
+                name="to"
+                label="Até"
+                type="time"
+              />
+            </div>
+          ))}
         </fieldset>
 
         <footer>
